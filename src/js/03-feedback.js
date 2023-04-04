25a2e52f2c9693ddef555ca70796953da9dcced9
DimaSvirin <115728392+DimaSvirin@users.noreply.github.com>
2023-04-04 12:09:43 +0300

    update task 1 and 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input',
+    throttle(element => {
+        formData[element.target.name] = element.target.value;
+        onInputFormValue();
+    }, 500)
+);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    // const {elements: (email, message)} = event.currentTarget;
+
+    if(email.value === "" || message.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+    throttle(onInputFormValue, 500);
+
+    event.currentTarget.reset();
+}
+
+function onInputFormValue() {
+    const inputValue = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, inputValue);
+
+}
+
+
+
 
