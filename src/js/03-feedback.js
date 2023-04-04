d3764355193879a1322a811973921b34d36c2235
DimaSvirin <115728392+DimaSvirin@users.noreply.github.com>
2023-04-04 12:49:54 +0300

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,49 @@
 import throttle from "lodash.throttle";
 
-const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
-
 const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.feedback-form input');
+const messageImput = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input',
-    throttle(element => {
-        formData[element.target.name] = element.target.value;
-        onInputFormValue();
-    }, 500)
-);
+form.addEventListener('input', onFormInput);
 
-function onFormSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    // const {elements: (email, message)} = event.currentTarget;
+function onFormInput() {
+    const formData = {
+        emailInput: emailInput.value,
+        messageImput: messageImput.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
-    if(email.value === "" || message.value === "") {
-        return alert("Please fill in all the fields!");
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    if(emailInput.value === '' || messageImput.value === '') {
+        const random = [
+            'Try again',
+            'Not enough text',
+            'Go on to type',
+            'I believe in you',
+        ]
+
+        for (let i of random) {
+            return alert(random[Math.floor(Math.random() * random.length)]);
+        }
     }
-    throttle(onInputFormValue, 500);
 
-    event.currentTarget.reset();
+    const saveLocalStorage = localStorage.getItem(STORAGE_KEY);
+    const objectFormLocalStorage = JSON.parse(saveLocalStorage);
+
+    localStorage.removeItem(STORAGE_KEY);
+    e.currentTarget.reset();
 }
 
-function onInputFormValue() {
-    const inputValue = JSON.stringify(formData);
-    localStorage.setItem(STORAGE_KEY, inputValue);
-
+function populateForm() {
+    const saveLocalStorage = localStorage.getItem(STORAGE_KEY);
+    const objectFormLocalStorage = JSON.parse(saveLocalStorage);
+    if(objectFormLocalStorage) {
+        emailInput.value = objectFormLocalStorage.emailInput;
+        messageImput.value = objectFormLocalStorage.messageImput
+    }
 }
-
-
-
-
